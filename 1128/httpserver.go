b7b1d30d7de666601fb0c74d9b6f628b14ca6191
yali <wowlili@163.com>
2021-11-28 21:07:36 +0800

    update

--- 1128/httpserver.go
+++ 1128/httpserver.go
@@ -1,28 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"runtime"
 	"strings"
+	"syscall"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
+	"github.com/woowlili/geeku_cloudnative/1128/metrics.go"
+
+	"github.com/wowlili/geeku_cloudnative/1128/metrics"
 )
 
 var Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
 
 func main() {
 	// zerolog.TimestampFunc().Local().Format("2006-01-02 15:04:05")
+	Logger.Info().Msg("Starting http server...")
+	metrics.Register()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Index)
 	mux.HandleFunc("/healthz", Healthz)
-	if err := http.ListenAndServe(":9999", mux); err != nil {
-		Logger.Fatal().Err(err).Msg("Startup failed")
-	}
+	mux.Handle("/metrics", promhttp.Hander())
 
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := http.Server{
+		Addr:    ":9999",
+		Handler: mux,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			Logger.Fatal().Err(err).Msg("Startup failed")
+		}
+	}()
+	Logger.Info().Msg("Server Started")
+	<-done
+	Logger.Info().Msg("Server Stoped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+	if err := srv.Shutdown(ctx); err != nil {
+		Logger.Fatal().Err(err).Msg("Server shutdown Failed")
+	}
+	Logger.Info().Msg("Server Exited Properly")
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
